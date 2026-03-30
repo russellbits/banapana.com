91fc9ec53b3ec7ea87458575b758d2c1a22edd20
russellbits <github@troped.com>
2026-03-30 11:23:52 -0400

    📑 Documents for brainstorm and Storybook.

---
+++ .storybook/PreviewWrapper.svelte
@@ -0,0 +1,20 @@
+<script>
+	let { children } = $props();
+</script>
+
+<div class="story-container">
+	{@render children()}
+</div>
+
+<style>
+	.story-container {
+		/* This forces the box height you're looking for */
+		min-height: 400px;
+		min-width: 400px;
+		height: 100%;
+		width: 100%;
+		display: block;
+		/* Optional: helps you see the container during debugging
+    outline: 1px dashed #ccc; */
+	}
+</style>
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,18 +1,21 @@
+import PreviewWrapper from './PreviewWrapper.svelte';
 import '../src/app.css';
 
 /** @type { import('@storybook/sveltekit').Preview } */
 const preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-    a11y: {
-      test: 'todo'
-    }
-  },
+	decorators: [() => PreviewWrapper],
+	parameters: {
+		layout: 'fullscreen', // Removes default padding that might cause clipping
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i
+			}
+		}
+	},
+	a11y: {
+		test: 'todo'
+	}
 };
 
 export default preview;
